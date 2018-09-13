df52318af199e425faf3d522346ce02ecd0363d1
GDY <dy@gdys-mbp.localdomain>
2018-09-13 21:59:31 +0200

    project added

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component }from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 
@@ -25,13 +25,13 @@ class App extends Component {
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter(robots => {
-      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase())
     })
-    if (this.state.robots.length === 0) {
-      return <h1>Loading</h1>
-    } else {
-      return (
+    return !robots.length ?
+       <h1>Loading</h1> :
+    (
         <div className='tc'>
           <h1 className='f2'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
@@ -43,7 +43,7 @@ class App extends Component {
     }
   }
   
-  }
+
   
   export default App;
   
