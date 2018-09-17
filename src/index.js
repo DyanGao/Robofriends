c45b377f0b60249cab6f3aa4b27dbfa639300ad4
GDY <dy@gdys-mbp.localdomain>
2018-09-17 22:32:37 +0200

    project added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 import 'tachyons'; 
 
 const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
 const store = createStore(
-  searchRobots, 
-  applyMiddleware(logger)
+  rootReducer, 
+  applyMiddleware(thunkMiddleware, logger)
   )
 
 ReactDOM.render(
--- src/reducers.js
+++ src/reducers.js
@@ -1,13 +1,37 @@
-import { CHANGE_SEARCHFIELD } from './constants.js';
+import { 
+  CHANGE_SEARCHFIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED
+} from './constants.js';
 
-const intialState = {
+const intialStateSearch = {
   searchField: ''
 }
 
-export const searchRobots = (state=intialState, action={}) => {
+export const searchRobots = (state=intialStateSearch, action={}) => {
   switch(action.type) {
     case CHANGE_SEARCHFIELD:
       return Object.assign({}, state, { searchField: action.payload });
+    default:
+      return state;
+  }
+}
+
+const intialStateRobots = {
+  isPending: false,
+  robots: [],
+  error: ''
+}
+
+export const requestRobots = (state=intialStateRobots, action={}) => {
+  switch(action.type) {
+    case REQUEST_ROBOTS_PENDING:
+      return Object.assign({}, state, { isPending: true })
+    case REQUEST_ROBOTS_SUCCESS:
+      return Object.assign({}, state, { robots: action.payload, isPending: false })
+    case REQUEST_ROBOTS_FAILED:
+      return Object.assign({}, state, { error: action.payload, isPending: false })
     default:
       return state;
   }
